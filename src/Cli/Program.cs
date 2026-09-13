3145ae7126e75bef151fe56620f5b750f84da107
Artur Podfedko <arturpodfedko@gmail.com>
2026-09-13 22:26:50 +0300

    lab01: fix json encoding and complete extra tasks

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,7 +1,36 @@
 ﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using System.Text.Encodings.Web;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+var appData = new
+{
+    Application = "CrossApp",
+    Student = "Подфедько Артур",
+    Group = "ФЕІ-37",
+    OSDescription = RuntimeInformation.OSDescription,
+    OSVersion = Environment.OSVersion.ToString(),
+    ProcessArchitecture = RuntimeInformation.ProcessArchitecture.ToString(),
+    DotNetVersion = Environment.Version.ToString(),
+    Runtime = RuntimeInformation.FrameworkDescription,
+    BaseDirectory = AppContext.BaseDirectory,
+    CurrentDirectory = Environment.CurrentDirectory,
+    Domain = "Замовлення (клієнт, товар, замовлення, рядок замовлення)"
+};
+
+if (args.Contains("--json"))
+{
+    var options = new JsonSerializerOptions 
+    { 
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping 
+    };
+    string jsonOutput = JsonSerializer.Serialize(appData, options);
+    Console.WriteLine(jsonOutput);
+}
+else
+{
 Console.WriteLine("Hi everyone");
 Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
 Console.WriteLine("Студент: Подфедько Артур, група ФЕІ-37");
@@ -15,3 +44,4 @@ Console.WriteLine($"Каталог застосунку   : {AppContext.BaseDire
 Console.WriteLine($"Поточний каталог     : {Environment.CurrentDirectory}");
 Console.WriteLine(new string('-', 52));
 Console.WriteLine("Предметна область: Замовлення (клієнт, товар, замовлення, рядок замовлення)");
+}
